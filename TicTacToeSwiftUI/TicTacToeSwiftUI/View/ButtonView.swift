0ec6c705be6c7293adaadeeb5648cd68d540806c
Mill3nium <milan-l97@hotmail.com>
2021-11-28 18:48:28 +0100

    Added separate ButtonView

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUI/View/ButtonView.swift
@@ -0,0 +1,37 @@
+//
+//  ButtonView.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Milan Languric on 2021-11-28.
+//
+
+import SwiftUI
+
+struct ButtonsView:View{
+    var body: some View{
+        HStack{
+            Section{
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "ear")
+                        .frame(width: 100, height: 50, alignment: .center)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "eye")
+                        .frame(width: 100, height: 50, alignment: .center)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                })
+            }.padding(.top,100)
+
+        }
+    }
+}
--- TicTacToeSwiftUI/TicTacToeSwiftUI/View/ContentView.swift
+++ TicTacToeSwiftUI/TicTacToeSwiftUI/View/ContentView.swift
@@ -49,7 +49,6 @@ struct ContentView: View {
                     }
                     .padding()
                     ButtonsView()
-                    
                 }
                             
             } else if orientation.isLandscape {
@@ -75,31 +74,4 @@ struct ContentView: View {
     }
 }
 
-struct ButtonsView:View{
-    var body: some View{
-        HStack{
-            Section{
-                Button(action: {
-                    
-                }, label: {
-                    Image(systemName: "ear")
-                        .frame(width: 100, height: 50, alignment: .center)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                })
-                
-                Button(action: {
-                    
-                }, label: {
-                    Image(systemName: "eye")
-                        .frame(width: 100, height: 50, alignment: .center)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                })
-            }.padding(.top,100)
 
-        }
-    }
-}
