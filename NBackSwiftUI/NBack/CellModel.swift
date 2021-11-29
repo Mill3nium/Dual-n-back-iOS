3156d7b34f1db702e7c1d2410ce8f8d74573a78b
Mill3nium <milan-l97@hotmail.com>
2021-11-29 17:07:47 +0100

    Added ability to change color on cells

---
+++ NBackSwiftUI/NBack/CellModel.swift
@@ -0,0 +1,8 @@
+//
+//  CellModel.swift
+//  NBack
+//
+//  Created by Milan Languric on 2021-11-29.
+//
+
+import Foundation
--- NBackSwiftUI/NBack/NBackVM.swift
+++ NBackSwiftUI/NBack/NBackVM.swift
@@ -6,6 +6,7 @@ import SwiftUI
 class NBackVM : ObservableObject  {
     let synthesizer = AVSpeechSynthesizer()
     var theModel = NBackModel()
+    
     @Published var orientation = UIDeviceOrientation.portrait
     @Published var markers : [VisualMarkerData] = initMarkers()
     @Published var playing = false
@@ -25,26 +26,37 @@ class NBackVM : ObservableObject  {
             synthesizer.speak(crossVoice)
         }
     }
+    
+    func turnOnMarker(index:Int){
+        markers[index].isTuredOn = true
+    }
+
+    func turnOffMarker(index:Int){
+        markers[index].isTuredOn = false
+    }
+
+    func isTurnedOn(){
+        
+    }
 }
 
 func initMarkers() -> [VisualMarkerData] {
     return [
-        VisualMarkerData(id: 0, state: 0, x: 0, y: 0),
-        VisualMarkerData(id: 1, state: 0, x: 1, y: 0),
-        VisualMarkerData(id: 2, state: 0, x: 2, y: 0),
-        VisualMarkerData(id: 3, state: 0, x: 0, y: 1),
-        VisualMarkerData(id: 4, state: 0, x: 1, y: 1),
-        VisualMarkerData(id: 5, state: 0, x: 2, y: 1),
-        VisualMarkerData(id: 6, state: 0, x: 0, y: 2),
-        VisualMarkerData(id: 7, state: 0, x: 1, y: 2),
-        VisualMarkerData(id: 8, state: 0, x: 2, y: 2)
+        VisualMarkerData(id: 0, isTuredOn: true, x: 0, y: 0),
+        VisualMarkerData(id: 1, isTuredOn: false, x: 1, y: 0),
+        VisualMarkerData(id: 2, isTuredOn: false, x: 2, y: 0),
+        VisualMarkerData(id: 3, isTuredOn: false, x: 0, y: 1),
+        VisualMarkerData(id: 4, isTuredOn: false, x: 1, y: 1),
+        VisualMarkerData(id: 5, isTuredOn: false, x: 2, y: 1),
+        VisualMarkerData(id: 6, isTuredOn: false, x: 0, y: 2),
+        VisualMarkerData(id: 7, isTuredOn: false, x: 1, y: 2),
+        VisualMarkerData(id: 8, isTuredOn: false, x: 2, y: 2)
     ]
 }
 
-
 struct VisualMarkerData: Hashable, Codable, Identifiable {
     var id: Int
-    var state : Int
+    var isTuredOn:Bool
     var x: Int
     var y: Int
 }
